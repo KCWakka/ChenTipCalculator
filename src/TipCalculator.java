0210b08606adcc8bfd9db571b5a2ca485b2d6a5e
BT_2N2_19 <sample@gmail.com>
2023-09-21 13:40:42 -0400

    Line changed.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,7 +3,7 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip Calculator!");
-        System.out.print("What is your income in dollars and cent?: ");
+        System.out.print("What is your income in dollars and cents?: ");
         double incomeNum = scan.nextDouble();
         scan.nextLine();
         System.out.print("How many people are in your group?: ");
