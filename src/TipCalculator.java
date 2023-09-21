b964686bf388c626caa15c8231937a87388e5c4b
BT_2N2_19 <sample@gmail.com>
2023-09-21 13:22:37 -0400

    Added a lot of scanner and starting to do loop

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,11 +3,18 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip Calculator!");
+        System.out.print("What is your income in dollars and cent?: ");
+        double incomeNum = scan.nextDouble();
+        scan.nextLine();
         System.out.print("How many people are in your group?: ");
         int peopleNum = scan.nextInt();
         scan.nextLine();
         System.out.print("What's the tip percentage? (0-100): ");
-        int tip = scan.nextInt();
-
+        int tipPercent = scan.nextInt();
+        scan.nextLine():
+        System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (put -1 to end): ");
+        double itemCost = scan.nextDouble();
+        scan.nextLine();
+        while (itemCost * 1 )
         }
 }
