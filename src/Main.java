0d5f120340aceb623bee7694c9e75d84c2b519f8
BT_2N2_19 <sample@gmail.com>
2023-09-21 13:10:11 -0400

    Added some line and scanner.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = Scanner(System.in);
+        System.out.println("Welcome to the tip Calculator!");
+        System.out.println("How many people are in your group?: ");
+        int peopleNum = scanner.nextInt;
+        scanner.nextLine;
+        System.out.println("What's the tip percentage? (0-100): ");
         }
 }
